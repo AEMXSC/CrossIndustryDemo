3c145fbba74b66e2d2cdaac56922ea3b93d22951
SuprabhatK <suprabhat.karmakar17@gmail.com>
2026-02-26 14:44:18 +0530

    changes in defination

--- blocks/product-compare-cards/product-compare-cards.js
+++ blocks/product-compare-cards/product-compare-cards.js
@@ -1,56 +1,44 @@
-
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
-  const colors = ['red', 'blue', 'black', 'white'];
-  let currentColorIndex = 0;
+  const cards = [...block.children];
 
-  const cardsContainer = document.createElement('div');
-  cardsContainer.className = 'cards-container';
+  block.innerHTML = '';
 
-  [...block.children].forEach((cardColumn) => {
-    const card = document.createElement('div');
-    card.className = 'card';
+  const container = document.createElement('div');
+  container.classList.add('product-compare-cards__grid');
 
-    // Apply initial color
-    card.style.backgroundColor = colors[currentColorIndex];
+  cards.forEach((card) => {
+    const image = card.querySelector('[data-name="image"]');
+    const title = card.querySelector('[data-name="title"]');
+    const description = card.querySelector('[data-name="description"]');
+    const specs = card.querySelector('[data-name="specs"]');
+    const ctaText = card.querySelector('[data-name="ctaText"]');
+    const ctaStyle = card.querySelector('[data-name="ctaStyle"]');
 
-    // Add color plate
-    const colorPlate = document.createElement('div');
-    colorPlate.className = 'color-plate';
-    colorPlate.innerHTML = `
-      <div class="color-option" style="background-color: red;"></div>
-      <div class="color-option" style="background-color: blue;"></div>
-      <div class="color-option" style="background-color: black;"></div>
-      <div class="color-option" style="background-color: white;"></div>
+    const newCard = document.createElement('div');
+    newCard.classList.add('product-card');
+
+    newCard.innerHTML = `
+      <div class="product-card__image">
+        ${image ? image.innerHTML : ''}
+      </div>
+      <h3 class="product-card__title">
+        ${title ? title.textContent : ''}
+      </h3>
+      <div class="product-card__description">
+        ${description ? description.innerHTML : ''}
+      </div>
+      <div class="product-card__specs">
+        ${specs ? specs.innerHTML : ''}
+      </div>
+      <div class="product-card__cta">
+        <a href="#" class="${ctaStyle ? ctaStyle.textContent : 'button'}">
+          ${ctaText ? ctaText.textContent : 'Learn More'}
+        </a>
+      </div>
     `;
 
-    // Add event listeners to color options
-    colorPlate.querySelectorAll('.color-option').forEach((option) => {
-      option.addEventListener('click', () => {
-        card.style.backgroundColor = option.style.backgroundColor;
-      });
-    });
-
-    // Move card content
-    while (cardColumn.firstElementChild) {
-      card.appendChild(cardColumn.firstElementChild);
-    }
-
-    // Add color plate to card
-    card.appendChild(colorPlate);
-
-    // Move instrumentation
-    moveInstrumentation(cardColumn, card);
-
-    // Add card to container
-    cardsContainer.appendChild(card);
-
-    // Update color index for next card
-    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    container.appendChild(newCard);
   });
 
-  // Replace block content
-  block.replaceChildren(cardsContainer);
+  block.appendChild(container);
 }
-    
